f99a977adec5224e815d6e4dffe3d7ac1350a2ed
Anna <anna.atsulus@gmail.com>
2025-09-29 10:05:29 +0300

    удалила лишние папки

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/developermail/GetMessaheResponse.java
+++
@@ -1,4 +0,0 @@
-package ru.stqa.mantis.manager.developermail;
-
-public record GetMessaheResponse(Boolean success, Object errors, String result) {
-}
--- sandbox/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Triangle perimetr = new Triangle(5.0, 5.0, 5.0);
-        System.out.println("Triangle perimetr is " + perimetr.getPerimetr());
-        System.out.println("Triangle area is " + perimetr.getArea());
-    }
-}
